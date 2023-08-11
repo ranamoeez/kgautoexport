ec6754b627c058df0efc91cdd686e3846230209f
Rana Moeez <ranamoeez740@gmail.com>
2023-08-11 02:39:30 -0700

    update

--- resources/views/admin/add-vehicle.blade.php
+++ resources/views/admin/add-vehicle.blade.php
@@ -669,7 +669,7 @@
                 var id = $(this).find("option:selected").val();
 
                 var settings = {
-                  "url": "http://localhost/kgexport/admin/get-auction-location/"+id,
+                  "url": "{{ url('admin/get-auction-location/') }}"+id,
                   "method": "GET",
                 };
 

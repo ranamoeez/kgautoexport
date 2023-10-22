6dedfe2ec9945842051f15f4680d27d2ed265274
Rana Moeez <link2moeez@gmail.com>
2023-10-23 02:42:28 +0500

    update

--- resources/views/admin/system-configuration/users.blade.php
+++ resources/views/admin/system-configuration/users.blade.php
@@ -503,7 +503,9 @@
                             $("#user-email").val(res.data.email);
                             $("#user-surname").val(res.data.surname);
                             $("#user-company").val(res.data.company);
-                            $("#country option[value="+res.data.country+"]").attr("selected", true);
+                            if (res.data.country !== "") {
+                                $("#country option[value="+res.data.country+"]").attr("selected", true);
+                            }
                             if (res.data.phone !== "" && res.data.phone !== null) {
                                 var phone = res.data.phone.split(" ");
                                 $("#user-phone").val(phone[1]);

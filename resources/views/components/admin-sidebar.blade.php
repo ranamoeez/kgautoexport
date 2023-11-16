b611cb1e03042e39603d259c1623c016a9cf75c4
Rana Moeez <link2moeez@gmail.com>
2023-11-16 11:26:57 +0500

    update

--- resources/views/components/admin-sidebar.blade.php
+++ resources/views/components/admin-sidebar.blade.php
@@ -1,3 +1,9 @@
+<style type="text/css">
+    .side_menu a {
+        padding: 15px 10px !important;
+    }
+</style>
+
 <div class="header-height d-flex">
 
     <a class="navbar-brand m-auto me-md-5" href="{{ url('/admin') }}">
@@ -8,19 +14,20 @@
     <ul class="list-unstyled" style="padding-top: 0px !important;">
         <li class="side_menu_item">
             <a href="javascript:void();" class="toggle-btn">
-                <i class="fas fa-bars"></i>
+                <i class="fas fa-bars" style="margin-left: 6px;"></i>
             </a>
         </li>
         @if(empty($auth_user->admin_level->access) || @in_array("2", json_decode($auth_user->admin_level->access)))
         <li class="side_menu_item">
             <a href="{{ url('admin/vehicles') }}" @if(@$type == 'vehicles') class="active" @endif>
-                <i class="fas fa-car"></i>
+                <i class="fas fa-car" style="margin-left: 6px; margin-right: 15px;"></i>
                 <span>Vehicles List</span>
             </a>
         </li>
         @endif
         <li class="side_menu_item">
             <a href="{{ url('admin/vehicles/add') }}" @if(@$type == 'add-vehicle') class="active" @endif>
+                <i class="fas fa-plus" style="font-size: 14px; margin-right: 3px; margin-top: 5px;"></i>
                 <i class="fas fa-car"></i>
                 <span>Add New Vehicle</span>
             </a>
@@ -28,23 +35,24 @@
         @if(empty($auth_user->admin_level->access) || @in_array("3", json_decode($auth_user->admin_level->access)))
         <li class="side_menu_item">
             <a href="{{ url('admin/containers') }}" @if(@$type == 'containers') class="active" @endif>
-                <i class="fas fa-box"></i>
-                <span style="margin-left: 5px;">Container List</span>
+                <i class="fas fa-box" style="margin-left: 7px; margin-right: 16px;"></i>
+                <span>Container List</span>
             </a>
         </li>
         @endif
         @if(empty($auth_user->admin_level->access) || @in_array("4", json_decode($auth_user->admin_level->access)))
         <li class="side_menu_item">
             <a href="{{ url('admin/containers/add') }}" @if(@$type == 'add-container') class="active" @endif>
+                <i class="fas fa-plus" style="font-size: 14px; margin-right: 3px; margin-top: 5px;"></i>
                 <i class="fas fa-box"></i>
-                <span style="margin-left: 5px;">Add new container</span>
+                <span>Add new container</span>
             </a>
         </li>
         @endif
         @if(empty($auth_user->admin_level->access) || @in_array("5.1", json_decode($auth_user->admin_level->access)))
         <li class="side_menu_item">
             <a href="{{ url('admin/financial-system') }}" @if(@$type == 'financial-system') class="active" @endif>
-                <i class="fas fa-money-bill"></i>
+                <i class="fas fa-money-bill" style="margin-left: 6px; margin-right: 15px;"></i>
                 <span>Financial System</span>
             </a>
         </li>
@@ -52,7 +60,7 @@
         @if(empty($auth_user->admin_level->access) || @in_array("6.1", json_decode($auth_user->admin_level->access)))
         <li class="side_menu_item">
             <a href="{{ url('admin/pickup-history') }}" @if(@$type == 'pickup-history') class="active" @endif>
-                <i class="fa-solid fa-list"></i>
+                <i class="fa-solid fa-list" style="margin-left: 6px; margin-right: 15px;"></i>
                 <span>Pickup History</span>
             </a>
         </li>
@@ -60,7 +68,7 @@
         @if(empty($auth_user->admin_level->access) || @in_array("7", json_decode($auth_user->admin_level->access)))
         <li class="side_menu_item">
             <a href="{{ url('admin/system-configuration/users') }}" @if(@$type == 'system-configuration') class="active" @endif>
-                <i class="fas fa-chart-bar"></i>
+                <i class="fas fa-chart-bar" style="margin-left: 6px; margin-right: 15px;"></i>
                 <span>System Configuration</span>
             </a>
         </li>
